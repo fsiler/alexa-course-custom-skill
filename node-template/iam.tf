c0c80ad14bd558d0fa36bd50cecb32f821fc91ef
Frank Siler <me@franksiler.com>
2025-01-26 12:22:39 -0600

    minor tf cleanup

--- node-template/iam.tf
+++ node-template/iam.tf
@@ -19,10 +19,10 @@ resource "aws_iam_role" "role_for_lambda" {
 # courtesy https://github.com/hashicorp/terraform/issues/9893#issuecomment-308306013
 # you will get a SkillManifestError if you don't have this
 resource "aws_lambda_permission" "with_alexa" {
-  statement_id  = "AllowExecutionFromAlexa"
-  action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.alexa_lambda.function_name}"
-  principal     = "alexa-appkit.amazon.com"
+  statement_id       = "AllowExecutionFromAlexa"
+  action             = "lambda:InvokeFunction"
+  function_name      = aws_lambda_function.alexa_lambda.function_name
+  principal          = "alexa-appkit.amazon.com"
 
-  event_source_token =  local.config.hows_your_day.skill.id
+  event_source_token = local.config.hows_your_day.skill.id
 }
